73a9ed199791f3ba7d2ec63744def5163c7f7644
Sean Cahill <seancahill25@gmail.com>
2018-05-13 23:54:15 +0000

    almost fininshed self assesment in mod 1

---
+++ edX/mod1/selfassesment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace university_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var UProgram = new UProgram("Information Technology", "Bachelors");
+
+            Console.WriteLine($"the Program is {UProgram.Class} and the degree it contains is {UProgram.Degree}");
+        }
+            
+    }
+    public class UProgram
+    {
+        public string Class { get; set; }
+        public string Degree { get; set; }
+   
+        public UProgram(string uclass, string degree)
+        {
+            this.Class = uclass;
+            this.Degree = degree;
+        }
+    }
+}
