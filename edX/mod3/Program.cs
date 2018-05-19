3d0059eb5de7608fe9863b86cfd441414b10d53a
Sean Cahill <seancahill25@gmail.com>
2018-05-19 16:19:14 +0000

    did mod3 lab1

---
+++ edX/mod3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+
+
+namespace Mod3_Lab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                streamReaderObject = new StreamReader("file1.txt");
+
+                String contents = streamReaderObject.ReadToEnd();
+
+                streamReaderObject.Close();
+
+                Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file cannot be found.");
+            }
+        }
+    }
+}
